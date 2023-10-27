a2391484bd50202906a38e28dc5fde2fead42220
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-10-27 13:05:46 +0200

    EmployeeIndexViewModel

--- MVCIntro/Data/MVCIntroContext.cs
+++ MVCIntro/Data/MVCIntroContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MVCIntro.Models;
+using MVCIntro.Models.ViewModels;
 
 namespace MVCIntro.Data
 {
@@ -15,6 +16,7 @@ namespace MVCIntro.Data
         }
 
         public DbSet<Employee> Employee => Set<Employee>();
+
 
        // public DbSet<Car> Car { get; set; } = default!;
     }
---
+++ MVCIntro/Models/ViewModels/EmployeeIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCIntro.Models.ViewModels
+{
+    public class EmployeeIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+    }
+}
